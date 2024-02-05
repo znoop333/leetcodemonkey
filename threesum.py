094c29347fbc48a6a0655f466553f369337f0567
David Johnson Thinkpad T16 <znoop@djt16>
2024-02-05 06:33:12 -0500

    start 3sum

---
+++ threesum.py
@@ -0,0 +1,29 @@
+import unittest
+from typing import List, Tuple
+
+
+def three_sum_answer1(nums: List[int]) -> List[Tuple]:
+  """ three_sum_answer1 solves the ThreeSum problem using brute force
+  :param nums: a list of integers in no particular order
+  :return: a list of elements of nums such that nums[i] + nums[j] + nums[k] == 0
+    and i != j != k. No duplicates of (nums[i], nums[j], nums[k]) are allowed.
+  """
+  solutions = set()
+  for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      for k in range(j + 1, len(nums)):
+        if nums[i] + nums[j] + nums[k] == 0:
+          t = [nums[i], nums[j], nums[k]]
+          t.sort()
+          solutions.add(t)
+  return list(solutions)
+
+
+class TestThreeSums(unittest.TestCase):
+  def test_two_sum_answer1(self):
+    ans = three_sum_answer1([-1, 0, 1, 2, -1, -4])
+    self.assertCountEqual(three_sum_answer1([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]])
+
+
+if __name__ == '__main__':
+  unittest.main()
