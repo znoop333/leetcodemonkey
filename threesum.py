bdb8e3090af8cd1418c80c8f0e58e95708895349
David Johnson Desktop2022 <djohn89@gmail.com>
2024-02-06 06:00:45 -0500

    reformat ans1

--- threesum.py
+++ threesum.py
@@ -13,14 +13,14 @@ def three_sum_answer1(nums: List[int]) -> List[Tuple[int]]:
     for j in range(i + 1, len(nums)):
       for k in range(j + 1, len(nums)):
         if nums[i] + nums[j] + nums[k] == 0:
-          t = [nums[i], nums[j], nums[k]]
-          t.sort()
+          t = sorted((nums[i], nums[j], nums[k]))
           solutions.add(tuple(t))
   return list(solutions)
 
 
 class TestThreeSums(unittest.TestCase):
   def test_two_sum_answer1(self):
+    self.assertCountEqual(three_sum_answer1([0, 1, 1]), [])
     self.assertCountEqual(three_sum_answer1([-1, 0, 1, 2, -1, -4]), [(-1, -1, 2), (-1, 0, 1)])
 
 
