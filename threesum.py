f57248eb4f51598ddc2b2d7ac838761bd2190e32
David Johnson Thinkpad T16 <znoop@djt16>
2024-02-08 06:45:22 -0500

    three_sum_answer2 done

--- threesum.py
+++ threesum.py
@@ -18,10 +18,32 @@ def three_sum_answer1(nums: List[int]) -> List[Tuple[int]]:
   return list(solutions)
 
 
+def three_sum_answer2(nums: List[int]) -> List[Tuple[int]]:
+  solutions = set()
+  nums.sort()
+  for i in range(len(nums)):
+    j, k = i + 1, len(nums) - 1
+    while j < k:
+      s = nums[i] + nums[j] + nums[k]
+      if s < 0: j += 1
+      elif s > 0: k -= 1
+      else:
+        solutions.add((nums[i], nums[j], nums[k]))
+        j += 1
+        # handle identical consecutive values
+        while nums[j - 1] == nums[j] and j < k:
+          j += 1
+  return list(solutions)
+
+
 class TestThreeSums(unittest.TestCase):
-  def test_two_sum_answer1(self):
+  def test_three_sum_answer1(self):
     self.assertCountEqual(three_sum_answer1([0, 1, 1]), [])
     self.assertCountEqual(three_sum_answer1([-1, 0, 1, 2, -1, -4]), [(-1, -1, 2), (-1, 0, 1)])
+
+  def test_three_sum_answer2(self):
+    self.assertCountEqual(three_sum_answer2([0, 1, 1]), [])
+    self.assertCountEqual(three_sum_answer2([-1, 0, 1, 2, -1, -4]), [(-1, -1, 2), (-1, 0, 1)])
 
 
 if __name__ == '__main__':
